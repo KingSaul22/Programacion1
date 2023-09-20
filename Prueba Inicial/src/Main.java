850022d417c35f0c77a457ea5c538660ff54dec7
Spatgar2210 <145562620+Spatgar2210@users.noreply.github.com>
2023-09-20 14:34:56 +0200

    Copia manual
    Prueba

---
+++ Prueba Inicial/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Hola, ¿cómo te llamas?");
+        String nombre = teclado.nextLine();
+        System.out.println("Encantado de conocerte, " + nombre + ". ¿Desde qué ciudad vienes?");
+        String ciudad = teclado.nextLine();
+        System.out.println("Wow, " + ciudad + " es una ciudad muy bonita");
+        System.out.println("Tanto " + nombre + " como " + ciudad + " son palabras que suena muy bien");
+    }
+
+}

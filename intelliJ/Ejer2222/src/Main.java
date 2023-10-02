d1e2658112a035d4b3ed523f76a11481d600934e
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-02 13:20:21 +0200

    2023-10-02 13:20

--- intelliJ/Ejer2222/src/Main.java
+++ intelliJ/Ejer2222/src/Main.java
@@ -2,17 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         int num;
 
         do {
             System.out.println("Introduzca un número entre 0 y 10");
-
-            num = Integer.parseInt(sc.nextLine());
+            while (!sc.hasNextInt()) {
+                System.out.println("El dato introducido no es valido.");
+                sc.next();
+            }
+            num = sc.nextInt();
         } while (num < 0 || num > 10);
+
+        sc.close();
+
         for (int i = 1; i <= 10; i++) {
 
-            System.out.printf("%2d * %2d = %3d\n", num, i, num * i);
+            System.out.printf("%2d * %2d = %3d\n", i, num, num * i);
         }
     }
 }

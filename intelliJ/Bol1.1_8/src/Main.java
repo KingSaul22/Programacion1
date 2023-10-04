5eb1062cdf4e845e338f0e397d6e18c89b302814
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-04 11:15:37 +0200

    11:15

--- intelliJ/Bol1.1_8/src/Main.java
+++ intelliJ/Bol1.1_8/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
+        float ans = 0;
 
         System.out.println("¿Cuantos números desea introducir?");
 
@@ -16,11 +17,13 @@ public class Main {
             num = sc.nextInt();
         } while (num <= 0);
 
-        System.out.println(num);
-
-        /*for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }*/
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Introduzca un número");
+            float numI = Float.parseFloat(sc.next());
+            if (numI < ans){
+                ans = numI;
+            }
+        }
+        System.out.println("El menos número es: " + ans);
     }
 }

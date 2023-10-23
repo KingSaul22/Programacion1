7b2f260431d606d4d7a4fe22ea7a6003438d5383
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-23 23:17:58 +0200

    Hasta el Ejer 5

--- intelliJ/Boletin 2.1/src/Ejer4/Main.java
+++ intelliJ/Boletin 2.1/src/Ejer4/Main.java
@@ -5,59 +5,53 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
-
-        int numMax = Integer.MIN_VALUE;
-        int numMin = Integer.MAX_VALUE;
-        float media = 0;
-        int maxR = 1, minR = 1;
-
         final int numRandom = 101;
 
-        int numArray = 6000;
-        int[] miArray = new int[numArray];
+        int posMD;
+        double media = 0, porcentaje = 0;
 
-        int[] miDigito = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] miDigito = new int[10];
+        for (int elem = 0; elem <= miDigito.length - 1; elem++) {
+            miDigito[elem] = 0;
+        }
+
+        final int numArray = 6000;
+        int[] miArray = new int[numArray];
 
         for (int elem = 0; elem <= miArray.length - 1; elem++) {
             miArray[elem] = r.nextInt(numRandom);
-            miArray[elem] = r.nextInt(numRandom);
 
             media += miArray[elem];
-            if (elem == 0) {
-                numMin = miArray[elem];
-                numMax = miArray[elem];
-            } else {
-                if (miArray[elem] > numMax) {
-                    numMax = miArray[elem];
-                    maxR = 1;
-                } else if (miArray[elem] < numMin) {
-                    numMin = miArray[elem];
-                    minR = 1;
-                } else if (miArray[elem] == numMax && miArray[elem] == numMin) {
-                    maxR++;
-                    minR++;
-                } else if (miArray[elem] == numMax) {
-                    maxR++;
-                } else if (miArray[elem] == numMin) {
-                    minR++;
-                }
-            }
+
+            posMD = miArray[elem] % 10;
+            miDigito[posMD]++;
         }
 
         media /= numArray;
 
-        if (maxR <= 1) {
-            System.out.printf("\nEl mayor valor de la matriz es: %d", numMax);
-        } else {
-            System.out.printf("\nEl mayor valor de la matriz es: %d y se repite %d veces", numMax, maxR);
+        for (int elem = 0; elem <= miDigito.length - 1; elem++) {
+            System.out.printf("Los números cuyo último dígito es %d, son un total de %d números.\n", elem, miDigito[elem]);
+            System.out.println("Esto equivale a un " + (double) miDigito[elem] / numArray * 100 + "%\n");
+            porcentaje += (double) miDigito[elem] / numArray * 100;
         }
 
-        if (minR <= 1) {
-            System.out.printf("\nEl menor valor de la matriz es: %d", numMin);
-        } else {
-            System.out.printf("\nEl menor valor de la matriz es: %d y se repite %d veces", numMin, minR);
-        }
+        System.out.println("La media de los números del vector es: " + media);
+        System.out.println("La suma de los porcentajes es: " + porcentaje + "% y su media es: " + porcentaje / 10 + "%");
 
-        System.out.println("\nY la media es: " + media);
+
+        /*for (int elem = 0; elem <= miDigito.length - 1; elem++) {
+            if (miDigito[elem] > 0) {
+                System.out.printf("Los números cuyo último dígito es %d, son un total de %d números.\n", elem, miDigito[elem]);
+                System.out.println("Esto equivale a un " + (double) miDigito[elem] / numArray * 100 + "%\n");
+                porcentaje += (double) miDigito[elem] / numArray * 100;
+            }
+        }
+        for (int elem = 0; elem <= miDigito.length - 1; elem++) {
+            if (miDigito[elem] == 0) {
+                System.out.printf("Los números cuyo último dígito es %d no se han repetido.\n", elem);
+                System.out.println("Esto equivale a un 0%\n");
+            }
+        }*/
     }
+
 }
---
+++ intelliJ/Boletin 2.1/src/Ejer5/Main.java
@@ -0,0 +1,24 @@
+package Ejer5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        final int numRandom = 1001;
+
+        System.out.println("¿Cuantos números desea introducir?");
+        int numArray = sc.nextInt();
+        sc.close();
+        System.out.println();
+
+        int[] miArray = new int[numArray];
+
+        for (int elem = 0; elem <= miArray.length - 1; elem++) {
+            miArray[elem] = r.nextInt(numRandom);
+            System.out.println(miArray[elem]);
+        }
+    }
+}

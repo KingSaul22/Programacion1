9602a66236ec1b2146f71862a02a1c57ec52d0d9
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-23 16:39:44 +0200

    23-10-2023 16:39

---
+++ intelliJ/Boletin 2.1/src/Ejer2/Main.java
@@ -0,0 +1,25 @@
+package Ejer2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+
+        System.out.println("¿Cuantos números desea introducir?");
+        int numArray = sc.nextInt();
+
+        int[] miArray = new int[numArray];
+        for (int elem = 0; elem <= miArray.length - 1; elem++) {
+            System.out.printf("\nIntroduzca un %dº número entero para colocar en la matriz\n", elem++);
+            elem--;
+            miArray[elem] = sc.nextInt();
+            if (miArray[elem] <= -1) {
+                ans += miArray[elem];
+            }
+        }
+        sc.close();
+        System.out.println("La suma de los elementos negativos de la matriz da como resultado: " + ans);
+    }
+}
---
+++ intelliJ/Boletin 2.1/src/Ejer3/Main.java
@@ -0,0 +1,26 @@
+package Ejer3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numMax=i, numMin, media;
+
+        System.out.println("¿Cuantos números desea introducir?");
+        int numArray = sc.nextInt();
+
+        int[] miArray = new int[numArray];
+        for (int elem = 0; elem <= miArray.length - 1; elem++) {
+            System.out.printf("\nIntroduzca un %dº número entero para colocar en la matriz: ", elem++);
+            elem--;
+            miArray[elem] = sc.nextInt();
+            
+
+
+
+        }
+        sc.close();
+        System.out.println("La suma de los elementos en posiciones pares de la matriz da como resultado: " + ans);
+    }
+}

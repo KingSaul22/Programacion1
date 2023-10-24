bfa3de292b9148fd6c24764db2731fe10438c6b3
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-24 10:16:57 +0200

    2023-10-24 10:16 Sigo con el Ejer 7

--- intelliJ/Boletin 2.1/src/Ejer7/Main.java
+++ intelliJ/Boletin 2.1/src/Ejer7/Main.java
@@ -1,7 +1,43 @@
 package Ejer7;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+    private static Random r = new Random();
+    final int numRandom = 1001;
     public static void main(String[] args) {
-        //Hola
+        final int numRandom = 1001;
+        int elem = -1;
+
+        System.out.println("Indica el tamaño del array:");
+        int numArray = sc.nextInt();
+        System.out.println("Indica el valor que deseas hallar:");
+        int numBuscar = sc.nextInt();
+        sc.close();
+        System.out.println();
+
+        int[] miArray = new int[numArray];
+
+        do {
+            elem++;
+            miArray[elem] = r.nextInt(numRandom);
+        } while (miArray[elem] != numBuscar && elem < numArray - 1);
+
+        if (numBuscar == miArray[elem]){
+            System.out.printf("El número %d, se encuentra en la posición %d del Array.", numBuscar, elem);
+        } else {
+            System.out.printf("El número %d, no se encuentra en el Array.", numBuscar);
+        }
+    }
+
+    private static void insertarValor(int[] arrayEnteros) {
+
+        for (int elem = 0; elem <= arrayEnteros.length - 1; elem++) {
+            arrayEnteros[elem] = r.nextInt(numRandom);
+
+        }
+
     }
 }

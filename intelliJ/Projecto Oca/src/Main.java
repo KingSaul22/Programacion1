8c6ed5de39b94acd51a5bf8cbc9c41f3e08b15a7
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-26 14:15:19 +0200

    Dado Funcicoinal + Jugador inicial

--- intelliJ/Projecto Oca/src/Main.java
+++ intelliJ/Projecto Oca/src/Main.java
@@ -7,16 +7,14 @@ public class Main {
     public static final int carasDado = 6;
 
     public static void main(String[] args) {
-        //int amarillo = 0, Verde = 1, Rojo = 2, Naranja = 3;
         int almacen = 0;
 
-        //int[] misJugadores = {1, 0, 0, 0};
-
+        String[] nombreJugadores = {"Amarillo", "Verde", "Rojo", "Naranja"};
         int[] primeraTirada = dadoAleatorio();
 
-        for (int elem = 0; elem < primeraTirada.length; elem++) {
+        for (int elem = 1; elem < primeraTirada.length; elem++) {
 
-            if (elem != 0 && primeraTirada[almacen] < primeraTirada[elem]) {
+            if (primeraTirada[almacen] < primeraTirada[elem]) {
 
                 misJugadores[almacen] = 0;
                 misJugadores[elem] = 1;
@@ -30,63 +28,31 @@ public class Main {
             }
         }
 
-        System.out.println("Tirada uno:");
-        System.out.println(primeraTirada[0]);
-        System.out.println(primeraTirada[1]);
-        System.out.println(primeraTirada[2]);
-        System.out.println(primeraTirada[3]);
-
-        System.out.println();
-        System.out.println();
-
-        /*for (int elem = 0; elem < misJugadores.length; elem++) {
-            if (misJugadores[elem] == 1) {
-                primeraTirada[elem] = miDado();
-                System.out.println(primeraTirada[elem]);
-            }
-        }*/
-
-
         while (repetirTirada()) {
 
             for (int elem = 0; elem < misJugadores.length; elem++) {
+
                 if (misJugadores[elem] == 1) {
                     primeraTirada[elem] = miDado();
-                    System.out.println(primeraTirada[elem]);
-                    System.out.println();
+                } else {
+                    primeraTirada[elem] = 0;
                 }
-            }
 
-            for (int elem = 0; elem < primeraTirada.length; elem++) {
-
-                if (elem != 0 && primeraTirada[almacen] < primeraTirada[elem]) {
-
-                    misJugadores[almacen] = 0;
-                    misJugadores[elem] = 1;
-                    almacen = elem;
-                }
             }
 
             for (int elem = almacen + 1; elem < primeraTirada.length; elem++) {
-                if (primeraTirada[almacen] == primeraTirada[elem]) {
-                    misJugadores[elem] = 1;
+
+                if (primeraTirada[almacen] > primeraTirada[elem]) {
+                    misJugadores[elem] = 0;
+                } else if (primeraTirada[almacen] < primeraTirada[elem]) {
+                    misJugadores[almacen] = 0;
+                    almacen = elem;
                 }
             }
         }
 
-        System.out.println(primeraTirada[0]);
-        System.out.println(primeraTirada[1]);
-        System.out.println(primeraTirada[2]);
-        System.out.println(primeraTirada[3]);
+        System.out.println("Empieza el jugador " + nombreJugadores[almacen]);
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-        System.out.println(misJugadores[0]);
-        System.out.println(misJugadores[1]);
-        System.out.println(misJugadores[2]);
-        System.out.println(misJugadores[3]);
 
     }
 
@@ -105,7 +71,7 @@ public class Main {
     }
 
     public static boolean repetirTirada() {
-        dadoIgual=0;
+        dadoIgual = 0;
 
         for (int elem = 0; elem < misJugadores.length; elem++) {
             if (misJugadores[elem] == 1) {

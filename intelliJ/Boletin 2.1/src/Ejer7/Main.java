b2b2669ddb829bc06274435c2ece6ee4d6ac18d9
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-10-31 14:17:04 +0100

    Boletín 2.1 - Ejer7 Completo

--- intelliJ/Boletin 2.1/src/Ejer7/Main.java
+++ intelliJ/Boletin 2.1/src/Ejer7/Main.java
@@ -1,4 +1,3 @@
-/*
 package Ejer7;
 
 import java.util.Random;
@@ -7,38 +6,44 @@ import java.util.Scanner;
 public class Main {
     private static Scanner sc = new Scanner(System.in);
     private static Random r = new Random();
-    final int numRandom = 1001;
+    private static final int numRandom = 1001;
+    private static int encontrado = 0;
+
     public static void main(String[] args) {
-        final int numRandom = 1001;
-        int elem = -1;
 
         System.out.println("Indica el tamaño del array:");
-        int numArray = sc.nextInt();
+        int tamArray = sc.nextInt();
         System.out.println("Indica el valor que deseas hallar:");
         int numBuscar = sc.nextInt();
         sc.close();
         System.out.println();
 
-        int[] miArray = new int[numArray];
+        int[] miArray = new int[tamArray];
 
-        do {
-            elem++;
-            miArray[elem] = r.nextInt(numRandom);
-        } while (miArray[elem] != numBuscar && elem < numArray - 1);
+        insertarValor(miArray);
 
-        if (numBuscar == miArray[elem]){
-            System.out.printf("El número %d, se encuentra en la posición %d del Array.", numBuscar, elem);
+        if (contieneValor(miArray, numBuscar)) {
+            System.out.printf("El número %d, se encuentra %d veces en el Array.", numBuscar, encontrado);
         } else {
             System.out.printf("El número %d, no se encuentra en el Array.", numBuscar);
         }
+
     }
 
     private static void insertarValor(int[] arrayEnteros) {
 
         for (int elem = 0; elem <= arrayEnteros.length - 1; elem++) {
+
             arrayEnteros[elem] = r.nextInt(numRandom);
-
         }
-
     }
-}*/
+
+    private static boolean contieneValor(int[] arrayEnteros, int buscarNum) {
+        for (int elem : arrayEnteros) {
+            if (elem == buscarNum) {
+                encontrado++;
+            }
+        }
+        return encontrado > 0;
+    }
+}
--- intelliJ/Boletin 2.1/src/Ejer8/Main.java
+++ intelliJ/Boletin 2.1/src/Ejer8/Main.java
@@ -1,4 +1,7 @@
 package Ejer8;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 }

9ef5c80517ce5a586b8f844dac3b9ab6c09a330f
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-05 00:44:28 +0100

    Boletin 2.1 Ejer16 Completo

--- intelliJ/Boletin 2.1/src/Ejer16/Main.java
+++ intelliJ/Boletin 2.1/src/Ejer16/Main.java
@@ -1,4 +1,34 @@
 package Ejer16;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static int numMax;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca hasta que número desea calcular las tablas de multiplicar:");
+        numMax = sc.nextInt();
+        sc.close();
+
+        int[][] matrizA = new int[numMax][10];
+
+        for (int fila = 0; fila < numMax; fila++) {
+
+            for (int colum = 0; colum < 10; colum++) {
+                matrizA[fila][colum] = (fila + 1) * (colum + 1);
+            }
+        }
+        for (int fila = 0; fila < numMax; fila++) {
+
+            for (int colum = 0; colum < 10; colum++) {
+                System.out.print(matrizA[fila][colum] + " | ");
+
+            }
+            System.out.println();
+        }
+
+    }
 }

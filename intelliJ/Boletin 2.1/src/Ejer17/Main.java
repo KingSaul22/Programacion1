71a4fedfba2fe25d6fb3ddc6b7c2940b67b007b4
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-05 01:00:59 +0100

    Boletin 2.1 Ejer17 Completo

--- intelliJ/Boletin 2.1/src/Ejer17/Main.java
+++ intelliJ/Boletin 2.1/src/Ejer17/Main.java
@@ -1,4 +1,36 @@
 package Ejer17;
 
+
 public class Main {
+    public static void main(String[] args) {
+        final int numFila = 10;
+
+        int[][] matrizA = new int[numFila][10];
+
+        for (int fila = 0; fila < numFila; fila++) {
+
+            for (int colum = 0; colum < 10; colum++) {
+                matrizA[fila][colum] = (fila + 1) * (colum + 1);
+            }
+        }
+         int[][] matrizB = matrizA;
+
+        /*for (int fila = 0; fila < matrizB.length; fila++) {
+
+            for (int colum = 0; colum < matrizB[fila].length; colum++) {
+                System.out.print(matrizB[fila][colum] + " | ");
+
+            }
+            System.out.println();
+        }*/
+
+        for (int[] fila : matrizB) {
+
+            for (int colum : fila) {
+                System.out.print(colum + " | ");
+
+            }
+            System.out.println();
+        }
+    }
 }

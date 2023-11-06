36258f61294ccbe2caaa8b06cb95b7df21e45cec
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-06 18:06:03 +0100

    Preparación Buscaminas y Examen Arrays 2020

--- intelliJ/Boletin 2.2/src/Ejer16/Main.java
+++ intelliJ/Boletin 2.2/src/Ejer16/Main.java
@@ -2,7 +2,7 @@ package Ejer16;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] miMatriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 2, 3}, {1, 5, 3}, {7, 5, 9}};
+        int[][] miMatriz = {{1, 2}, {4, 6}, {8, 9}, {1, 3}, {6, 3}, {7, 9}};
         imprimirMatriz(miMatriz);
 
         int[][] posPares = casillasPares(miMatriz);
@@ -19,16 +19,16 @@ public class Main {
                 for (int miniF = fila - 1; miniF < fila + 2; miniF++) {
                     for (int miniC = colum - 1; miniC < colum + 2; miniC++) {
 
-                        if (miniF >= 0 && miniC >= 0 && miniF < matriz.length && miniC < matriz[0].length){
+                        if (miniF >= 0 && miniC >= 0 && miniF < matriz.length && miniC < matriz[0].length) {
 
-                            if (matriz[miniF][miniC] % 2 == 0){
+                            if (matriz[miniF][miniC] % 2 == 0) {
                                 paresADY++;
                             }
 
                         }
                     }
                 }
-                if (matriz[fila][colum] % 2 ==0){
+                if (matriz[fila][colum] % 2 == 0) {
                     paresADY--;
                 }
 

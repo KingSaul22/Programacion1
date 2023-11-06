3d0ffd3e96b1db86eaa69c2170d61716a16e3d6b
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-06 16:26:55 +0100

    Boletin 2.2 Ejer15 Completado

--- intelliJ/Boletin 2.2/src/Ejer15/Main.java
+++ intelliJ/Boletin 2.2/src/Ejer15/Main.java
@@ -1,4 +1,48 @@
 package Ejer15;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Indique la fila a revisar:");
+        int posFila = 0;
+        System.out.println("Indique la columna a revisar:");
+        int posColum = 2;
+        System.out.println();
+
+        int[][] miMatriz = new int[6][6];
+        int i = 1;
+        for (int fila = 0; fila < miMatriz.length; fila++) {
+            for (int colum = 0; colum < miMatriz[0].length; colum++) {
+                miMatriz[fila][colum] = i;
+                i++;
+            }
+        }
+        imprimirMatriz(miMatriz);
+        System.out.println();
+
+        int posicion = numPosicion(miMatriz, posColum, posFila);
+
+        System.out.println(posicion);
+    }
+
+    public static int numPosicion(int[][] matriz, int x, int y) {
+        int posicion = 1;
+
+        posicion += y * matriz.length;
+
+        posicion += x;
+
+
+        return posicion;
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        System.out.print("\nLa salida es:\n");
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int colum = 0; colum < matriz[0].length; colum++) {
+
+                System.out.printf(" %2d", matriz[fila][colum]);
+            }
+            System.out.println();
+        }
+    }
 }

9d80d43c9729433eb5c123e986521902d2c60ac0
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-06 15:39:12 +0100

    Boletin 2.2 Ejer14 Completado

--- intelliJ/Boletin 2.2/src/Ejer14/Main.java
+++ intelliJ/Boletin 2.2/src/Ejer14/Main.java
@@ -3,9 +3,10 @@ package Ejer14;
 public class Main {
     public static void main(String[] args) {
         int[][] miMatriz = {{1,2,3},{4,5,6}};
+        imprimirMatriz(miMatriz);
 
         int[][] matrizB = matrizFtoC(miMatriz);
-
+        imprimirMatriz(matrizB);
     }
 
     public static int[][] matrizFtoC(int[][] matriz) {
@@ -14,10 +15,21 @@ public class Main {
         for (int fila = 0; fila < matriz.length; fila++) {
             for (int colum = 0; colum < matriz[0].length; colum++) {
 
-                matrizInv[fila][colum] = matriz[fila][colum];
+                matrizInv[colum][fila] = matriz[fila][colum];
             }
         }
 
         return matrizInv;
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        System.out.print("\nLa salida es:\n");
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int colum = 0; colum < matriz[0].length; colum++) {
+
+                System.out.print(" " + matriz[fila][colum]);
+            }
+            System.out.println();
+        }
     }
 }

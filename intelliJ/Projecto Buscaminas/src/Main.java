a5ef3ae96da4c2e61241405b099fb13fd8d2bf75
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-13 20:02:02 +0100

    Project Buscamina Beta [0.0.2] Todo en progreso

--- intelliJ/Projecto Buscaminas/src/Main.java
+++ intelliJ/Projecto Buscaminas/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int[][] tablero;
+    public static int tamTabla = 65;
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Indique la dificultad del juego.");
+
+        String ans = sc.nextLine();
+
+
+        generarTablero(ans);
+    }
+
+    public static void generarTablero(String dificultad) {
+        switch (dificultad){
+            case "facil":
+                tamTabla = 10;
+                break;
+            case "moderado":
+                tamTabla = 16;
+                break;
+            case "dificil":
+                tamTabla = 22;
+                break;
+            case "imposible":
+                tamTabla = 26;
+        }
+        tablero = new int[tamTabla][tamTabla];
+
     }
 }

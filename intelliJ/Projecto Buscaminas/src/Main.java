926d1de76c8ec8b93e5d9d4ca436637249469370
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-14 13:47:13 +0100

    Project Buscaminas Beta [0.1.0] Funcionalidad para escoger dificultad completa
    TODO: Rellenar el tablero base con las coordenadas y minas.

--- intelliJ/Projecto Buscaminas/src/Main.java
+++ intelliJ/Projecto Buscaminas/src/Main.java
@@ -16,6 +16,8 @@ public class Main {
         int[][] tablero = new int[tamTabla + 1][tamTabla + 1];
         imprimirMatriz(tablero);
 
+        rellenoBase(tablero);
+
     }
 
     public static void imprimirMatriz(int[][] matriz) {
@@ -27,39 +29,35 @@ public class Main {
     }       //Imprime una matriz dada.
 
     public static void modoJuego(String dificultad) {
+        int pcMina = 0;
 
         switch (dificultad) {   //TODO Simplificar el Switch y extraer lo que se pueda.
             case "facil":
-                //System.out.println("\nEl modo de juego seleccionado es 'Facil'.\nSe procederá a crear una tabla con un tamaño de 10x10.\nUn 10% de sus casillas serán minas.\n");
                 tamTabla = 10;
-                numMina = 10;
+                pcMina = 10;
                 break;
             case "moderado":
-                //System.out.println("\nEl modo de juego seleccionado es 'Moderado'.\nSe procederá a crear una tabla con un tamaño de 16x16.\nUn 15% de sus casillas serán minas.\n");
                 tamTabla = 16;
-                numMina = (int) ((tamTabla ^ 2) * 0.15);
+                pcMina = 15;
                 break;
             case "dificil":
-                //System.out.println("\nEl modo de juego seleccionado es 'Dificil'.\nSe procederá a crear una tabla con un tamaño de 22x22.\nUn 25% de sus casillas serán minas.\n");
                 tamTabla = 22;
-                numMina = (int) ((tamTabla ^ 2) * 0.25);
+                pcMina = 25;
                 break;
             case "imposible":
-                //System.out.println("\nEl modo de juego seleccionado es 'Imposible'.\nSe procederá a crear una tabla con un tamaño de 26x26.\nUn 35% de sus casillas serán minas.\n");
                 tamTabla = 26;
-                numMina = (int) ((tamTabla ^ 2) * 0.35);
+                pcMina = 35;
                 break;
             case "custom":
                 System.out.println("Introduzca el tamaño del lado usado para el tablero cuadrado:");
                 tamTabla = sc.nextInt();
                 System.out.println("Introduzca el porcentaje de minas que se crearán:");
-                double pcMina = sc.nextDouble() / 100;
-                numMina = (int) ((tamTabla ^ 2) * pcMina);
-                //System.out.println("\nIniciando modo de juego customizado.\nSe procederá a crear una tabla con un tamaño de " + tamTabla + "x" + tamTabla + ".\nUn " + (int) (pcMina * 100) + "% de sus casillas serán minas.\n");
+                pcMina = sc.nextInt();
         }
 
-        System.out.println("\nIniciando modo de juego " + dificultad + ".\nSe procederá a crear una tabla con un tamaño de " + tamTabla + "x" + tamTabla + ".\nUn " + (int) (pcMina * 100) + "% de sus casillas serán minas.\n");
-    }
+        numMina = (tamTabla ^ 2) * pcMina / 100;
+        System.out.println("\nIniciando modo de juego " + dificultad + ".\nSe procederá a crear una tabla con un tamaño de " + tamTabla + "x" + tamTabla + ".\nUn " + pcMina + "% de sus casillas serán minas.\n");
+    }       //Ajusta parametros según dificultad seleccionada
 
     public static void rellenoBase(int[][] matriz) {
 

2e1d4d79f63b69c1e43f4156875df4e03c980f6e
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-11-23 14:45:36 +0100

    Preparación Boletin 3.1 - POO

---
+++ intelliJ/Boletin 3.1/src/Ejer1/Rectangulo.java
@@ -0,0 +1,20 @@
+package Ejer1;
+
+public class Rectangulo {
+    private double longitud, ancho;
+
+    public Rectangulo() {
+        longitud = 1;
+        ancho = 1;
+    }
+
+    private void setLado(int a) {
+
+        longitud = a;
+    }
+
+    private void setAncho(int ancho) {
+
+        this.ancho = ancho;
+    }
+}

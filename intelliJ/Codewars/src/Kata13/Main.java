0fd2cad28fdda13b58a5753da0d781cf963f4081
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2023-12-03 19:18:12 +0100

    Codewars - Kata 13 Enunciado

---
+++ intelliJ/Codewars/src/Kata13/Main.java
@@ -0,0 +1,41 @@
+package Kata13;
+
+import java.util.List;
+
+/**
+ * In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+ * <p>
+ * Create as many "shufflings" as you can!
+ * <p><p>
+ * Examples:
+ * <p>
+ * With input 'a':
+ * <p>
+ * Your function should return: ['a']
+ * <p>
+ * With input 'ab':
+ * <p>
+ * Your function should return ['ab', 'ba']
+ * <p>
+ * With input 'abc':
+ * <p>
+ * Your function should return ['abc','acb','bac','bca','cab','cba']
+ * <p>
+ * With input 'aabb':
+ * <p>
+ * Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+ * <p>
+ * Note: The order of the permutations doesn't matter.
+ * <p><p>
+ * Good luck!
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> singlePermutations(String s) {
+        // Your code here!
+        return null;
+    }
+}

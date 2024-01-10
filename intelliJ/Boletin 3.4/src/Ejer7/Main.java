9a292e05b192005b5500b21baa642d48a2c476d3
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-01-10 23:08:35 +0100

    Boletin 3.4 - Ejer7

---
+++ intelliJ/Boletin 3.4/src/Ejer7/Main.java
@@ -0,0 +1,40 @@
+package Ejer7;
+
+import java.util.Scanner;
+
+import static java.lang.Character.toLowerCase;
+
+public class Main {
+    public static void main(String[] args) {
+        //if ("h".matches())
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca la frase.\n");
+        String frase = sc.nextLine();
+        sc.close();
+
+        System.out.println("\n" + moverVocales(frase));
+
+    }
+
+    private static String moverVocales(String cadena) {
+        StringBuilder str = new StringBuilder(cadena.replaceAll("\\s+", ""));
+        int auxCadena = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            //if (toLowerCase(str.charAt(auxCadena)) == [aeiou]){
+            //TODO: No detecta acentos.
+            if (String.valueOf(toLowerCase(str.charAt(auxCadena))).matches("[aeiou]")) {
+
+                str.append(str.charAt(auxCadena));
+                str.deleteCharAt(auxCadena);
+            } else {
+                auxCadena++;
+            }
+        }
+
+
+        return String.valueOf(str);
+    }
+}

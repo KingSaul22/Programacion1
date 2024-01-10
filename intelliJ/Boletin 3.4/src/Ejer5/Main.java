ea0f2b6e302de491b580fb04caf1f0ec95f28a40
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-01-10 12:47:29 +0100

    Boletin 3.4 - Ejer 5 Proceso

--- intelliJ/Boletin 3.4/src/Ejer5/Main.java
+++ intelliJ/Boletin 3.4/src/Ejer5/Main.java
@@ -1,4 +1,32 @@
 package Ejer5;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Introduzca una frase original: ");
+        String sentence = sc.nextLine();
+
+        System.out.printf("Introduzca el texto a buscar: ");
+        String word1 = sc.nextLine();
+
+        System.out.printf("Introduzca el texto que reemplaza: ");
+        String word2 = sc.nextLine();
+
+
+        System.out.println("\n" + reemplazaExpresion(sentence, word1, word2));
+    }
+
+    private static String reemplazaExpresion(String frase, String reemplazado, String reemplazador) {
+
+        //TODO: Revisar última letra error.
+        for (int i = frase.indexOf(reemplazado); i != -1; i += reemplazador.length()) {
+
+        }
+
+        return frase;
+    }
 }

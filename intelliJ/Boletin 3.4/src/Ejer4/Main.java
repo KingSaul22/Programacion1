b89c768c38bf190cdde934305616a78c10567427
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-01-10 11:15:30 +0100

    Boletin 3.4 - Ejer 4 proceso

---
+++ intelliJ/Boletin 3.4/src/Ejer4/Main.java
@@ -0,0 +1,20 @@
+package Ejer4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca una frase");
+        String frase = sc.nextLine();
+
+        System.out.println("Introduzca la palabra escondida");
+        String word = sc.nextLine();
+
+        int auxWord = 0;
+        for (int i = 0; i < frase.length(); i++) {
+
+        }
+    }
+}

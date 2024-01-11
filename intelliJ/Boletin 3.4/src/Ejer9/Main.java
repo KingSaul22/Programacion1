0eb735327def95da8f5452bf7cf14cda97110f84
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-01-11 14:43:41 +0100

    Boletin 3.4 - Ejer 9

--- intelliJ/Boletin 3.4/src/Ejer9/Main.java
+++ intelliJ/Boletin 3.4/src/Ejer9/Main.java
@@ -12,12 +12,26 @@ public class Main {
         sentence = "Frase con números, 12 entre las palabras 456 y al final 1000";
         sc.close();
 
-        System.out.print("\nEl total de la suma de los números comprendidos en la cadena ");
-        System.out.printf("'%s' es de: %d", sentence, sumaDesdeString(sentence));
+        System.out.print("\nEl total de la suma de los números comprendidos en la cadena:\n");
+        System.out.printf("'%s'\nEs de: %d", sentence, sumaDesdeString(sentence));
     }
 
 
-    private static int sumaDesdeString (String cadena) {
+    private static int sumaDesdeString(String cadena) {
+        int suma = 0;
+        int auxNumero = 0;
 
+        for (int i = 0; i < cadena.length(); i++) {
+
+            if (Character.isDigit(cadena.charAt(i))) {
+                auxNumero = auxNumero * 10 + Integer.parseInt(String.valueOf(cadena.charAt(i)));
+
+            } else {
+                suma += auxNumero;
+                auxNumero = 0;
+            }
+        }
+
+        return suma + auxNumero;
     }
 }

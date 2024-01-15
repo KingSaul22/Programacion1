9e16ed29b9ad8c2bfa855153e85cc8319053ee87
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-01-15 14:03:22 +0100

    Boletin 3.4 - Ejer 11 Proceso

--- intelliJ/Boletin 3.4/src/Ejer11/Main.java
+++ intelliJ/Boletin 3.4/src/Ejer11/Main.java
@@ -1,13 +1,30 @@
 package Ejer11;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final int MAX_INTENTOS = 7;
+    private static String word;
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int intento = 1;
 
+        do {
+            System.out.println("Introduzca una letra");
+            if (isValidLetter(sc.nextLine().charAt(0))) {
+
+            }
+
+        } while (intento <= MAX_INTENTOS);
     }
 
-    private static char getLetter(){
+    private static boolean isValidLetter(char letter) {
 
+        return word.contains(String.valueOf(letter));
     }
 
-    private static
+    //private static boolean stringContentLetter(char letter) {
+
+    //}
 }

fc1fdb5d642fc440ed5352719a3c0591409a81a0
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-02-01 18:39:51 +0100

    Boletin 4.1 - Ejer 3 Completo

--- intelliJ/Boletin 4.1/src/Ejer3/Main.java
+++ intelliJ/Boletin 4.1/src/Ejer3/Main.java
@@ -1,26 +1,74 @@
 package Ejer3;
 
+import EntradaSalida.Lectora;
+import static java.lang.Character.toUpperCase;
+
 public class Main {
     public static void main(String[] args) {
-        Mago a, b;
-        Clerigo c;
+        Mago a = null, b = null;
+        Clerigo c = null;
 
         try {
-            a = new Mago("Carlos Juan", TRaza.ORCO, 15, 20, 100);
+            a = new Mago(selectNombre(), selectRaza(), 15, 20, 100);
         } catch (PersonajeException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
 
         try {
-            b = new Mago("Rodolfo", TRaza.ENANO, 12, 19, 76);
+            b = new Mago("Rodolfo", selectRaza(), 12, 19, 76);
         } catch (PersonajeException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
 
         try {
-            c = new Clerigo("Rodolfa", TRaza.ENANO, 20, 15, 70, "Pepe");
+            c = new Clerigo("Rodolfa", b.getRaza(), 20, 15, 70, "Pepe");
         } catch (PersonajeException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            a.aprendeHechizo(selectNombre());
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            a.aprendeHechizo(selectNombre());
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            b.aprendeHechizo(selectNombre());
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(a);
+        System.out.println(b);
+
+        try {
+            a.lanzaHechizo(selectNombre(), b);
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            b.lanzaHechizo(selectNombre(), a);
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            c.lanzaCuracion(b);
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            a.lanzaHechizo(selectNombre(), b);
+        } catch (PersonajeException e) {
+            System.out.println(e.getMessage());
         }
 
         System.out.println(a);
@@ -29,12 +77,32 @@ public class Main {
     }
 
     private static String selectNombre() {
+        String nombre = Lectora.solicitarCadenaMinus("Escriba el nombre deseado: ").trim();
 
+        while (nombre.isBlank() || nombre.length() > 15) {
+            nombre = Lectora.solicitarCadenaMinus("Escriba un nombre valido e inferior en longitud a 15 caracteres: ").trim();
+        }
+
+        return nombre.replaceFirst(String.valueOf(nombre.charAt(0)), String.valueOf(toUpperCase(nombre.charAt(0))));
     }
 
-    private static String selectRaza() {
+    private static TRaza selectRaza() {
+        System.out.print("""
+                                
+                Introduzca la raza de su personaje:
+                  1. Humano
+                  2. Elfo
+                  3. Enano
+                  4. Orco
+                """
+        );
 
+        return switch (Lectora.leerEnteroDeRango("Selección: ", 1 + TRaza.values().length, 0)) {
+            case 1 -> TRaza.HUMANO;
+            case 2 -> TRaza.ELFO;
+            case 3 -> TRaza.ENANO;
+            default -> TRaza.ORCO;
+        };
     }
-    
 
 }

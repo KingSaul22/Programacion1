e008df568d18fc3ce30ffae9a213b3d5be45dded
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-02-01 17:45:35 +0100

    Boletin 4.1 - Ejer 3 Proceso Prueba incial completa TODO: Completar metodos con interacción del usuario

--- intelliJ/Boletin 4.1/src/Ejer3/Main.java
+++ intelliJ/Boletin 4.1/src/Ejer3/Main.java
@@ -1,4 +1,40 @@
 package Ejer3;
 
 public class Main {
+    public static void main(String[] args) {
+        Mago a, b;
+        Clerigo c;
+
+        try {
+            a = new Mago("Carlos Juan", TRaza.ORCO, 15, 20, 100);
+        } catch (PersonajeException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            b = new Mago("Rodolfo", TRaza.ENANO, 12, 19, 76);
+        } catch (PersonajeException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            c = new Clerigo("Rodolfa", TRaza.ENANO, 20, 15, 70, "Pepe");
+        } catch (PersonajeException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+    private static String selectNombre() {
+
+    }
+
+    private static String selectRaza() {
+
+    }
+    
+
 }

217bd0e952e5c55a1a0ab275cd2eee9791e5efc6
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-02-20 18:53:00 +0100

    Examen Herencia 2122 - Corrección Matriz en FicheroTexto

--- intelliJ/ExamHerencia-2122/src/FicheroTexto.java
+++ intelliJ/ExamHerencia-2122/src/FicheroTexto.java
@@ -10,17 +10,17 @@ public class FicheroTexto extends Fichero implements Convertible {
     }
 
     public void setContenido(String[] contenido) {
-        this.contenido = new String[1][contenido.length];
+        this.contenido = new String[contenido.length][1];
 
-        for (int i = 0; i < contenido.length; i++) this.contenido[0][i] = contenido[i];
+        for (int i = 0; i < contenido.length; i++) this.contenido[i][0] = contenido[i];
         //System.arraycopy(contenido, 0, this.contenido[0], 0, contenido.length);
     }
 
     @Override
     public long getSize() {
         StringBuilder cadenaConjunta = new StringBuilder();
-        for (int i = 0; i < contenido[0].length; i++){
-            if (contenido[0][i] != null) cadenaConjunta.append(contenido[0][i]);
+        for (int i = 0; i < contenido.length; i++){
+            if (contenido[i][0] != null) cadenaConjunta.append(contenido[i][0]);
         }
 
         return String.valueOf(cadenaConjunta).getBytes(StandardCharsets.UTF_8).length;
@@ -33,8 +33,8 @@ public class FicheroTexto extends Fichero implements Convertible {
     @Override
     public String textToAudio() {
         StringBuilder cadenaConjunta = new StringBuilder();
-        for (int i = 0; i < contenido[0].length; i++){
-            if (contenido[0][i] != null) cadenaConjunta.append(i + 1).append(". ").append(contenido[0][i]).append(" ");
+        for (int i = 0; i < contenido.length; i++){
+            if (contenido[i][0] != null) cadenaConjunta.append(i + 1).append(". ").append(contenido[i][0]).append(" ");
         }
 
         //return String.valueOf(cadenaConjunta);

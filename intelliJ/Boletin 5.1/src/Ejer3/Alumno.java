a241299bd5655d8ba29695e5529b892d9bdcc2be
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-03-06 16:16:53 +0100

    Boletin 5.1 - Ejer 3 Alumno actualizado

--- intelliJ/Boletin 5.1/src/Ejer3/Alumno.java
+++ intelliJ/Boletin 5.1/src/Ejer3/Alumno.java
@@ -4,18 +4,22 @@ import java.time.LocalDate;
 
 public class Alumno extends Persona {
     private static final int MAYORIA_EDAD = 18;
-    private LocalDate fechaNacimiento;
+    private final LocalDate FECHA_NACIMIENTO;
 
     public Alumno(String nombre, LocalDate fecha) {
         super(nombre);
-        fechaNacimiento = fecha;
+        FECHA_NACIMIENTO = fecha;
     }
 
     @Override
     public void sendMessage(Persona receptor, String mensaje) throws MensajeException {
         LocalDate fechaMinima = LocalDate.now().minusYears(MAYORIA_EDAD);
+<<<<<<< HEAD
+        if (receptor instanceof Alumno && FECHA_NACIMIENTO.isAfter(fechaMinima)) {
+=======
         //if (receptor.getClass() == Alumno.class && fechaNacimiento.isAfter(fechaMinima)) {
         if (receptor instanceof Alumno && fechaNacimiento.isAfter(fechaMinima)) {
+>>>>>>> a7edd94b2ad590562a6a902d9e8c545564c17765
             throw new MensajeException("Los alumnos menores de edad no pueden enviar mensajes a otros alumnos");
         }
 

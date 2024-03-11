9b29e91d90ad6fe120491fb0ff47b1af555af0ab
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-03-11 14:45:32 +0100

    Boletin 5.1 - Ejer 7 Proceso Recetario.java

--- intelliJ/Boletin 5.1/src/Ejer7/Model/Recetario.java
+++ intelliJ/Boletin 5.1/src/Ejer7/Model/Recetario.java
@@ -1,4 +1,17 @@
 package Ejer7.Model;
 
+import Ejer7.Excepciones.RecetaException;
+
+import java.util.HashMap;
+
 public class Recetario {
+    private HashMap<Receta, > recetas;
+
+    public Recetario() {
+        this.recetas = new HashMap();
+    }
+
+    public void aniadirReceta(Receta recetaNueva) throws RecetaException {
+        
+    }
 }

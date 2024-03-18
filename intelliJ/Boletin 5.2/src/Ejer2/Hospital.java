c456bb8b3fbb2aebe06f42f3666285aeb7acb738
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-03-18 11:14:45 +0100

    Boletin 5.2 - Ejer 2 Proceso - Hospital
    Hospital#getPacientesOrdenAtender()
    Hospital#atenderPaciente()
    Hospital#getNumPacienteSize()
    Hospital#getPacientesOrdenPorEdad()

--- intelliJ/Boletin 5.2/src/Ejer2/Hospital.java
+++ intelliJ/Boletin 5.2/src/Ejer2/Hospital.java
@@ -6,24 +6,46 @@ import java.util.List;
 
 public class Hospital {
     private List<Paciente> pacientes;
-    private int numPaciente;
+    private static Integer numPaciente;
 
     public Hospital() {
         pacientes = new LinkedList<>();
-        numPaciente = 0;
+        numPaciente = 1;
     }
 
     public void aniadirPaciente(Paciente paciente) {
         int posPaciente=0;
         Iterator<Paciente> it = pacientes.iterator();
         while(it.hasNext()){
-            posPaciente++;
             Paciente pc = it.next();
             if (paciente.compareTo(pc) < 0) {
                 pacientes.add(posPaciente, paciente);
                 //TODO: repasar lógica de posPaciente y el .add()
                 break;
             }
+            posPaciente++;
         }
+    }
+
+    /**
+     * Este método aprovecha los flujos para devolver una copia de la cola  de pacientes,
+     * esta cola ya esta ordenada de manera natural por el orden en que se atenderan
+     * @return Una copia de la {@link Hospital#pacientes cola de pacientes}
+     */
+    public List<Paciente> getPacientesOrdenAtender() {
+        //return pacientes.stream().toList();
+        return (List<Paciente>) (((LinkedList<Paciente>) pacientes).clone());
+    }
+
+    public Paciente atenderPaciente() {
+        return ((LinkedList<Paciente>) pacientes).poll();
+    }
+
+    public int getNumPacienteSize() {
+        return pacientes.size();
+    }
+
+    public List<Paciente> getPacientesOrdenPorEdad() {
+        return pacientes.stream().sorted((a, b) -> b.getEdad() - a.getEdad()).toList();
     }
 }

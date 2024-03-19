73d5d8bab3b95177510ca3dbe63ddd51c83f3790
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-03-19 12:39:05 +0100

    Examen Colecciones 2020/21 - Ejer1 Completo
    Principal -> añadido: NetPlease#listadoDeTodasPeliculasDeTodosLosTemas()

--- intelliJ/ExamColec2021/src/Ejer1/Principal.java
+++ intelliJ/ExamColec2021/src/Ejer1/Principal.java
@@ -2,24 +2,20 @@ package Ejer1;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		
-		NetPlease netPlease=new NetPlease();
-		
-		
-		try {
-			netPlease.annadirTema("COMEDIA");
-			netPlease.annadirTema("ACCION");
-			netPlease.annadirTema("DRAMA");
-			
-			netPlease.addPelicula("COMEDIA", new Pelicula("BAJO EL MISMO TECHO", 2019));
-			
-			
-			
-		} catch (NetPleaseException e) {
-			System.err.println(e.getMessage());
-		}
-		
-	}
+    public static void main(String[] args) {
+        NetPlease netPlease = new NetPlease();
 
+        try {
+            netPlease.annadirTema("COMEDIA");
+            netPlease.annadirTema("ACCION");
+            netPlease.annadirTema("DRAMA");
+
+            netPlease.addPelicula("COMEDIA", new Pelicula("BAJO EL MISMO TECHO", 2019));
+
+            netPlease.listadoDeTodasPeliculasDeTodosLosTemas();
+
+        } catch (NetPleaseException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }

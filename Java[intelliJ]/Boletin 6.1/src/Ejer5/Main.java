74cfa5aa12c5183b889ff4118def03c79fc042fa
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-04-09 12:45:01 +0200

    Boletin 6.1 - Ejer 5 Proceso

---
+++ Java[intelliJ]/Boletin 6.1/src/Ejer5/Main.java
@@ -0,0 +1,17 @@
+package Ejer5;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("""
+                
+                1. Crear Directorio
+                2. Crear Fichero de Texto
+                3. Borrar Fichero de Texto
+                4. Mostrar Ficheros de una Carpeta
+                5. Salir""");
+    }
+}

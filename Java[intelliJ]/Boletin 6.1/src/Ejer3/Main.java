e3e0ab863f4ed9daa9c656060cdecf2d37be5ceb
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-04-10 10:50:48 +0200

    Boletin 6.1 - Ejer 3 y 4 Completos

--- Java[intelliJ]/Boletin 6.1/src/Ejer3/Main.java
+++ Java[intelliJ]/Boletin 6.1/src/Ejer3/Main.java
@@ -5,13 +5,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         //IO; NIO en el Ejer4
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(".\\Resources\\Ejer3\\salidaEj3.txt", true), 22);
+        /*try (BufferedWriter bw = new BufferedWriter(new FileWriter(".\\Resources\\Ejer3\\salidaEj3.txt", true), 22);
              BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
             String input = br.readLine();
             while (!input.equals("fin")) {
                 bw.write(input);
                 input = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }*/
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter pw = new PrintWriter(new FileWriter(".\\Resources\\Ejer3\\salidaEj3.txt", true))) {
+
+            String input;
+            while (!(input = br.readLine()).equals("fin")) {
+                pw.println(input);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
--- Java[intelliJ]/Boletin 6.1/src/Ejer4/Main.java
+++ Java[intelliJ]/Boletin 6.1/src/Ejer4/Main.java
@@ -2,21 +2,21 @@ package Ejer4;
 
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) {
         //NIO; IO en el Ejer3
         Path outPath = Paths.get(".\\Resources\\Ejer4\\salidaEj4.txt");
-        try (BufferedWriter outData = Files.newBufferedWriter(outPath);
-             BufferedReader inData = new BufferedReader(new InputStreamReader(System.in))) {
-
-            String data = inData.readLine();
-            while (!data.equals("fin")) {
-                outData.write(data);
-                data = inData.readLine();
+        try (BufferedReader inData = new BufferedReader(new InputStreamReader(System.in))) {
+            String data;
+            while (!(data = inData.readLine()).equals("fin")) {
+                //Files.write(outPath, data.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+                Files.writeString(outPath, data, StandardOpenOption.APPEND);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());

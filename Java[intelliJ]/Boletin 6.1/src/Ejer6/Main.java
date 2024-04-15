0001fd97b6c220ee84d6920700238c6350103012
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-04-15 13:15:22 +0200

    Boletin 6.1 - Ejer 6 Extras

--- Java[intelliJ]/Boletin 6.1/src/Ejer6/Main.java
+++ Java[intelliJ]/Boletin 6.1/src/Ejer6/Main.java
@@ -2,9 +2,12 @@ package Ejer6;
 
 import utils.MiEntradaSalida;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,18 +15,33 @@ public class Main {
     }
 
     private static void mostrarContenidosCarpeta() {
-        File directorio = getValidDirectorio().toFile();
+        Path directorio = getValidDirectorio();
 
-        File[] listFiles = directorio.listFiles();
-        if (listFiles == null) return;
-
-        System.out.println("Ficheros dentro de la carpeta " + directorio.getName() + ":");
-        for (File file : listFiles) {
-            if (file.isDirectory()) {
-                System.out.println(file.getName());
-            } else {
-                System.out.println(file.getName() + " ("+file.length()+" bytes)");
-            }
+        System.out.println("Ficheros dentro de la carpeta " + directorio.getFileName() + ":");
+        //try (Stream<Path> directorios = Files.list(Paths.get("/Users/Alumno/Desktop"))) {
+        try (Stream<Path> directorios = Files.list(directorio)) {
+            directorios.forEach(a -> {
+                        if (Files.isRegularFile(a)) {
+                            try {
+                                System.out.println(a.getFileName() + " (" + Files.size(a) / 1024.0 + " KB)");
+                            } catch (IOException e) {
+                                System.out.println(a.getFileName() + " (Peso no detectado)");
+                            }
+                        } else if (Files.isDirectory(a)) {
+                            System.out.println(a.getFileName() + " (Directorio)");
+                        } else {
+                            System.out.println(a.getFileName() + " (Otro)");
+                        }
+                    }
+            );
+        } catch (NotDirectoryException e) {
+            System.out.println("Directorio no encontrado");
+            System.out.println(e.getMessage());
+        } catch (SecurityException e) {
+            System.out.println("Sin permisos suficientes para leer el directorio");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -31,7 +49,7 @@ public class Main {
         String name = MiEntradaSalida.leerCadena("Introduzca el nombre del directorio a mostrar");
         Path outPath = Paths.get(".\\Resources\\" + name);
 
-        while (!outPath.toFile().isDirectory()) {
+        while (!Files.isDirectory(outPath)) {
             System.out.println("El nombre del directorio " + name + " no existe");
             name = MiEntradaSalida.leerCadena("Introduzca otro nombre para el directorio a mostrar");
             outPath = Paths.get(".\\Resources\\" + name);

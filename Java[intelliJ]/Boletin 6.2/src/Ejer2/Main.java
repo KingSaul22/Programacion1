a510018b3c78eaddf7ef34e6f5a5fef0f7e53a06
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-04-19 13:02:31 +0200

    Boletin 6.2 - Ejer 2 Proceso
    Se ha creado la clase SalidaEntradaNIO.java con el fin de crear métodos que realicen procesos de aceptación y validación en lo relacionado a ficheros

---
+++ Java[intelliJ]/Boletin 6.2/src/Ejer2/Main.java
@@ -0,0 +1,8 @@
+package Ejer2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Java[intelliJ]/Boletin 6.2/src/utils/SalidaEntradaNIO.java
@@ -0,0 +1,52 @@
+package utils;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+public class SalidaEntradaNIO {
+
+    public static Path getValidFichero(String base) {
+        String name = MiEntradaSalida.leerCadena("Introduzca el nombre del fichero con las matriculas");
+        Path outPath = Paths.get(base + name);
+
+        while (!Files.isRegularFile(outPath)) {
+            System.out.println("El nombre del archivo " + name + " no existe");
+            name = MiEntradaSalida.leerCadena("Introduzca otro nombre para el archivo a leer");
+            outPath = Paths.get(base + name);
+        }
+
+        return outPath;
+    }
+
+    public static void mostrarInteriorDirectorio(Path directorio) {
+        try (Stream<Path> pathStream = Files.list(directorio)) {
+            pathStream.forEach(a -> {
+                System.out.print(a.getFileName());
+                if (Files.isDirectory(a)) {
+                    System.out.println(" (directorio)");
+                } else if (Files.isRegularFile(a)) {
+                    try {
+                        System.out.println(" (" + Files.size(a) / 1024.0 + " KB)");
+                    } catch (IOException e) {
+                        System.out.println(" (peso no detectado)");
+                    }
+                } else {
+                    System.out.println(" (otro)");
+                }
+            });
+        } catch (NotDirectoryException e) {
+            System.out.println("La ruta indicada no es un directorio");
+            System.out.println(e.getMessage());
+        } catch (SecurityException e) {
+            System.out.println("No se puede acceder al fichero");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static boolean validarDirectorio(Path directorio) {
+        return Files.isDirectory(directorio);
+    }
+}

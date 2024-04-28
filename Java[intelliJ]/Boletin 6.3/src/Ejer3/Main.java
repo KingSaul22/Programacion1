1fd5adba8aba867df0e2bf4302e6f6d945c95a26
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-04-29 00:10:39 +0200

    Boletin 6.3 - Ejer 3 Proceso

--- Java[intelliJ]/Boletin 6.3/src/Ejer3/Main.java
+++ Java[intelliJ]/Boletin 6.3/src/Ejer3/Main.java
@@ -2,13 +2,19 @@ package Ejer3;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
     public static void main(String[] args) {
@@ -49,8 +55,53 @@ public class Main {
             System.out.println("\nAgregación del atributo id");
             for (int i = 0; i < platos.getLength(); i++) {
                 Element plato = (Element) platos.item(i);
+                Node parent = plato.getParentNode();
+                parent.removeChild(plato);
                 plato.setAttribute("id", String.valueOf(i + 1));
+                parent.appendChild(plato);
             }
+
+
+            //PARTE CUATRO
+            Element platoNuevo = doc.createElement("food");
+            Element dataPlatoNuevo = doc.createElement("name");
+            dataPlatoNuevo.setTextContent("Churros Bermudo");
+            platoNuevo.appendChild(dataPlatoNuevo);
+            dataPlatoNuevo = doc.createElement("price");
+            dataPlatoNuevo.setTextContent("1.50€");
+            platoNuevo.appendChild(dataPlatoNuevo);
+            dataPlatoNuevo = doc.createElement("description");
+            dataPlatoNuevo.setTextContent("Unos deliciosos churros, hechos con todo el amor del profesor a sus alumnos");
+            platoNuevo.appendChild(dataPlatoNuevo);
+            dataPlatoNuevo = doc.createElement("calories");
+            dataPlatoNuevo.setTextContent("500");
+            platoNuevo.appendChild(dataPlatoNuevo);
+
+            File outFile = new File(".\\src\\Ejer3\\desayunoB.xml");
+            Files.deleteIfExists(outFile.toPath());
+            Files.createFile(outFile.toPath());
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            StreamResult streamResult = new StreamResult(outFile);
+            DOMSource source = new DOMSource(doc);
+            transformer.transform(source, streamResult);
+
+
+            //PARTE CINCO
+            platos = doc.getElementsByTagName("food");
+            for (int i = 0; i < platos.getLength(); i++) {
+                Element plato = (Element) platos.item(i);
+                if (Integer.parseInt(plato.getAttribute("calories")) > 500) {
+                    plato.getParentNode().removeChild(plato);
+                }
+            }
+            File outFileB = new File(".\\src\\Ejer3\\desayuno_saludable.xml");
+            Files.deleteIfExists(outFileB.toPath());
+            Files.createFile(outFileB.toPath());
+            streamResult = new StreamResult(outFileB);
+            transformer.transform(source, streamResult);
 
         } catch (IllegalArgumentException e) {
             System.out.println("No se reconoce el fichero asignado\n" + e.getMessage());
@@ -59,7 +110,11 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Error de lectura del archivo\n" + e.getMessage());
         } catch (SAXException e) {
-            System.out.println("Error de saxe del parser\n" + e.getMessage());
+            System.out.println("Error de SAX del parser\n" + e.getMessage());
+        } catch (TransformerConfigurationException e) {
+            System.out.println("Error en la configuración del transformer\n" + e.getMessage());
+        } catch (TransformerException e) {
+            System.out.println("Error al transformar\n" + e.getMessage());
         }
     }
 }
---
+++ Java[intelliJ]/Boletin 6.3/src/Ejer3b/Main.java
@@ -0,0 +1,187 @@
+package Ejer3b;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    private static Document doc;
+
+    public static void main(String[] args) {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            doc = db.parse(".\\src\\Ejer3b\\desayuno.xml");
+        } catch (IllegalArgumentException e) {
+            System.out.println("No se reconoce el fichero asignado\n" + e.getMessage());
+        } catch (ParserConfigurationException e) {
+            System.out.println("Error de configuración del parser\n" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error de lectura del archivo\n" + e.getMessage());
+        } catch (SAXException e) {
+            System.out.println("Error de SAX del parser\n" + e.getMessage());
+        }
+
+        //PARTE UNO
+        getDesayunosRangoPrecio(0, 5);
+        System.out.println();
+
+        //PARTE DOS
+        getDesayunosRangoCalorias(0, 500);
+        System.out.println();
+
+        //PARTE TRES
+        addIdToPlato();
+
+        //PARTE CUATRO
+        Element churros = newPlato(
+                "Churros Bermudo",
+                1.50,
+                "Unos deliciosos churros, hechos con todo el amor del profesor a sus alumnos",
+                500);
+        aniadirPlato(churros);
+
+        //PARTE CINCO
+        copyDesayunosSaludables(true);
+    }
+
+    private static void aniadirPlato(Element plato) {
+        String nameBase = plato.getElementsByTagName("name").item(0).getTextContent();
+        NodeList platos = doc.getElementsByTagName("food");
+        for (int i = 0; i < platos.getLength(); i++) {
+            Element nPlato = (Element) platos.item(i);
+            if (nPlato.getElementsByTagName("name").item(0).getTextContent().equals(nameBase)) {
+                System.out.println("Ya existe un plato con el nombre " + nameBase);
+                return;
+            }
+        }
+
+        int lastId = getLastIntIdFood();
+        plato.setAttribute("id", String.valueOf(++lastId));
+        platos.item(platos.getLength() - 1).appendChild(plato);
+        //TODO: Guardar Nuevo Plato
+    }
+
+    private static void getDesayunosRangoPrecio(double min, double max) {
+        NodeList platos = doc.getElementsByTagName("food");
+        if (platos == null) return;
+
+        System.out.println("Lista de platos con un precio dentro del rango [" + min + ", " + max + "]:");
+        for (int i = 0; i < platos.getLength(); i++) {
+            Element plato = (Element) platos.item(i);
+            String precioStr = plato.getElementsByTagName("price").item(0)
+                    .getTextContent().replace(',', '.');
+            double precio = Double.parseDouble(precioStr.substring(0, precioStr.length() - 1));
+            if (precio >= min && precio <= max) {
+                System.out.println("  · " + plato.getElementsByTagName("name").item(0).getTextContent() +
+                        " (" + plato.getElementsByTagName("price").item(0).getTextContent() + ")");
+            }
+        }
+    }
+
+    private static void getDesayunosRangoCalorias(int min, int max) {
+        NodeList platos = doc.getElementsByTagName("food");
+        if (platos == null) return;
+
+        System.out.println("Lista de platos con unas calorias dentro del rango [" + min + ", " + max + "]:");
+        for (int i = 0; i < platos.getLength(); i++) {
+            Element plato = (Element) platos.item(i);
+            int calorias = Integer.parseInt(plato.getElementsByTagName("calories").item(0).getTextContent());
+            if (calorias >= min && calorias <= max) {
+                System.out.println("  · " + plato.getElementsByTagName("name").item(0).getTextContent() +
+                        " (" + plato.getElementsByTagName("calories").item(0).getTextContent() + ")");
+            }
+        }
+    }
+
+    private static void addIdToPlato() {
+        NodeList platos = doc.getElementsByTagName("food");
+        if (platos == null) return;
+
+        for (int i = 0; i < platos.getLength(); i++) {
+            Element plato = (Element) platos.item(i);
+            plato.setAttribute("id", String.valueOf(i + 1));
+        }
+    }
+
+    private static Element newPlato(String nombre, double precio, String descripcion, int calorias) {
+        Element plato = doc.createElement("food");
+
+        Element nombrePlato = doc.createElement("name");
+        nombrePlato.setTextContent(nombre);
+        plato.appendChild(nombrePlato);
+
+        Element precioPlato = doc.createElement("price");
+        precioPlato.setTextContent(precio + "€");
+        plato.appendChild(precioPlato);
+
+        Element descripcionPlato = doc.createElement("description");
+        descripcionPlato.setTextContent(descripcion);
+        plato.appendChild(descripcionPlato);
+
+        Element caloriasPlato = doc.createElement("calories");
+        caloriasPlato.setTextContent(String.valueOf(calorias));
+        plato.appendChild(caloriasPlato);
+
+        return plato;
+    }
+
+    private static void copyDesayunosSaludables(final boolean sobreEscribir) {
+        File outFile = new File(".\\src\\Ejer3b\\desayuno_saludable.xml");
+        if (outFile.exists() && !sobreEscribir) {
+            System.out.println("Ya existe el desayuno saludable");
+            return;
+        }
+
+
+        NodeList platos = doc.getElementsByTagName("food");
+        for (int i = 0; i < platos.getLength(); i++) {
+            Element plato = (Element) platos.item(i);
+            if (Integer.parseInt(plato.getElementsByTagName("calories").item(0).getTextContent()) > 500) {
+                plato.getParentNode().removeChild(plato);
+                i--;
+            }
+        }
+
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            StreamResult streamResult = new StreamResult(outFile);
+
+            DOMSource source = new DOMSource(doc);
+
+            transformer.transform(source, streamResult);
+            System.out.println("Fichero 'desayuno_saludable.xml' creado con exito");
+        } catch (TransformerConfigurationException e) {
+            System.out.println("Error de configuración del transformer\n" + e.getMessage());
+        } catch (TransformerException e) {
+            System.out.println("Error al transformar\n" + e.getMessage());
+        }
+    }
+
+    private static int getLastIntIdFood() {
+        int last = 0;
+        NodeList platos = doc.getElementsByTagName("food");
+        for (int i = 0; i < platos.getLength(); i++) {
+            Element plato = (Element) platos.item(i);
+            int id = Integer.parseInt(plato.getAttribute("id"));
+            if (last < id) {
+                last = id;
+            }
+        }
+
+        return last;
+    }
+}

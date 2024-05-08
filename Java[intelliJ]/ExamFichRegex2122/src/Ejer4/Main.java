d1c7152bfb69ee758207d361c7eb4c0c7c2fc7e4
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2024-05-08 14:15:55 +0200

    Examen Ficheros y Regex 2021/22 - Completo

--- Java[intelliJ]/ExamFichRegex2122/src/Ejer4/Main.java
+++ Java[intelliJ]/ExamFichRegex2122/src/Ejer4/Main.java
@@ -1,12 +1,12 @@
 package Ejer4;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import utilsXML.AyudasXml;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     private static Document doc;
@@ -23,7 +23,31 @@ public class Main {
     }
 
     private static void continentesVuelos() {
-        Map<Integer, List<Node>> continentes = new HashMap<>();
-        //TODO: Ejer 4 - Extra
+        Map<Integer, Set<Node>> continentes = new HashMap<>();
+        
+        NodeList ids = doc.getElementsByTagName("id");
+        for (int i = 0; i < ids.getLength(); i++) {
+            continentes.put(Integer.parseInt(String.valueOf(ids.item(i).getTextContent().charAt(0))), new HashSet<>());
+        }
+
+        NodeList vuelos = doc.getElementsByTagName("vuelo");
+        for (int i = 0; i < vuelos.getLength(); i++) {
+            Element vuelo = (Element) vuelos.item(i);
+            continentes.get(Integer.parseInt(String.valueOf(ids.item(i).getTextContent().charAt(0)))).add(vuelo);
+        }
+
+        String baseOutRuta = "./src/Ejer4/continente";
+        for (int codContinente : continentes.keySet()) {
+            Document newDoc = AyudasXml.crearDocumento();
+            if (newDoc == null) continue;
+            newDoc.appendChild(newDoc.createElement("vuelos"));
+            newDoc.getDocumentElement().setAttribute("continente", String.valueOf(codContinente));
+            for (Node vuelo : continentes.get(codContinente)) {
+                newDoc.getDocumentElement().appendChild(newDoc.importNode(vuelo, true));
+            }
+            if (AyudasXml.guardarDocumento(newDoc, baseOutRuta + codContinente + ".xml")) {
+                System.out.println("Creado el fichero para el continente con código " + codContinente);
+            }
+        }
     }
 }
